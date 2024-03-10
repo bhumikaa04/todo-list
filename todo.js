d7e8a6b51db1dd39e2b5653c35ff710a92eca13a
Bhumika <136867118+bhumikaa04@users.noreply.github.com>
2024-03-10 20:08:38 +0530

    Updated todo.js

--- todo.js
+++ todo.js
@@ -61,3 +61,74 @@ function showPage(desiredPage){
     var selectedPage = document.getElementById(desiredPage);
         selectedPage.classList.add('show');
 }
+// saving and storing data in the list
+
+function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML) ; 
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data") ; 
+}
+
+showTask(); 
+console.log("website was refreshed.")
+
+// navigation in between the tabs 
+
+function showPage(desiredPage){
+    var showtab = document.querySelector('.show');
+    if(showtab){
+        showtab.classList.remove('show');
+        showtab.classList.add('page');
+    }
+    var selectedPage = document.getElementById(desiredPage); 
+    if (selectedPage) {
+        selectedPage.classList.add('show');
+        selectedPage.classList.remove('page');
+    }
+
+}
+ 
+// storing the data in the titleBar 
+
+function saveTitleBar(){
+    localStorage.setItem("dataTitleBar" , titleBar.innerHTML); 
+}
+
+function showTitleBar(){
+    titleBar.innerHTML = localStorage.getItem("dataTitleBar") ; 
+}
+console.log("website was refreshed."); 
+
+// creating a new list
+
+function createList(){
+    let titleBar = document.querySelector('.titleBar');
+
+    // Check if titleBar is valid before proceeding
+    if (!titleBar) {
+        console.error('Error: titleBar is not a valid DOM element');
+        return;
+    }
+    let newList = document.createElement('div');
+    titleBar.appendChild(newList) ; 
+    newList.classList.add('titleContent') ; 
+    newList.classList.add('List2')
+    newList.innerHTML="List2" ; 
+    console.log(newList);
+    saveTitleBar() ; 
+
+    let newPage = document.createElement('div') ; 
+    newPage.classList.add('container');
+    newPage.classList.add('page');  
+    newPage.setAttribute('id' , 'page3') ; 
+    console.log(newPage); 
+
+    console.log(titleBar);
+    // not working portion . 
+    newList.addEventListener('click' , showPage('page3')) ;
+    saveTitleBar();
+
+    console.log(titleBar.innerHTML);
+}
